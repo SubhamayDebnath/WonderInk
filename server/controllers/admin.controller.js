38a754b42fcea34f0c4e7761a4c44dac05cf8a24
SubhamayDebnath <subhamaydebnath6@gmail.com>
2024-10-08 20:39:01 +0530

    "Updated Tailwind CSS file with new classes and modified existing ones; refactored admin controller to include user model and render user page; updated admin dashboard and header templates with new links and styling."

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -1,40 +1,47 @@
+import User from "../models/user.model.js";
 const adminLayout = "../views/layouts/admin";
 /* 
     Dashboard
 */
-const dashboard=async (req,res,next) => {
-    try {
-        const metaData = {
-          title: "Dashboard - WonderInk",
-          description: "Welcome to Dashboard",
-        };
-        if(!req.cookies.token){
-            res.redirect("/auth/login")
-          }else{
-            res.render("admin/index", { metaData, layout: adminLayout });
-          }
-        // res.render("admin/index", { metaData });
-    } catch (error) {
-        console.error("Dashboard: ", error);
-        res.status(500).json({
-          message: "An unexpected error occurred. Please try again later.",
-        });
+const dashboard = async (req, res, next) => {
+  try {
+    const metaData = {
+      title: "Dashboard - WonderInk",
+      description: "Welcome to Dashboard",
+    };
+    if (!req.cookies.token) {
+      res.redirect("/auth/login");
+    } else {
+      res.render("admin/index", { metaData, layout: adminLayout });
     }
-}
-const getAllUsers=async (req,res,next) => {
-    try {
-        // const metaData = {
-        //   title: "Home Page",
-        //   description: "Welcome to our home page",
-        // };
-        // res.render("admin/index", { metaData });
-       res.send("hello")
-    } catch (error) {
-        console.error("Get all users: ", error);
-        res.status(500).json({
-          message: "An unexpected error occurred. Please try again later.",
-        });
+  } catch (error) {
+    console.error("Dashboard: ", error);
+    res.status(500).json({
+      message: "An unexpected error occurred. Please try again later.",
+    });
+  }
+};
+/* 
+    Get all users
+*/
+const getAllUsers = async (req, res, next) => {
+  try {
+    const metaData = {
+      title: "All users - WonderInk",
+      description: "Welcome to Dashboard",
+    };
+    const users = await  User.find();
+    if (!req.cookies.token) {
+      res.redirect("/auth/login");
+    } else {
+      res.render("admin/user", { metaData, layout: adminLayout,users });
     }
-}
+  } catch (error) {
+    console.error("Get all users: ", error);
+    res.status(500).json({
+      message: "An unexpected error occurred. Please try again later.",
+    });
+  }
+};
 
-export { dashboard ,getAllUsers}
+export { dashboard, getAllUsers };
