39e3261fcdd591fa118ab1fb8e3bedf7cfe936a4
Subhamay Debnath <subhamaydebnath@gmail.com>
2024-11-27 19:31:06 +0530

    added

--- server/controllers/main.controller.js
+++ server/controllers/main.controller.js
@@ -9,7 +9,19 @@ const homePage = async (req,res) => {
         console.log(`Home page error : ${error}`);
     }
 }
+const blogsPage=async (req,res) => {
+    try {
+        const locals = {
+            title: "Wonderink - Blogs",
+            description: "Welcome to our Blogs page",
+        };
+        res.render('home/blogs',{locals})
+    } catch (error) {
+        console.log(`Blogs page error : ${error}`);
+    }
+}
 
 export{
-    homePage
+    homePage,
+    blogsPage
 }
--- server/routes/home.routes.js
+++ server/routes/home.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { homePage } from "../controllers/main.controller.js";
+import { homePage,blogsPage } from "../controllers/main.controller.js";
 
 const router = Router()
 
 router.get("/",homePage);
+router.get('/blogs',blogsPage)
 
 export default router;
