6244e21d220e0a029941d936ecb9f12b54d722a5
Subhamay Debnath <subhamaydebnath@gmail.com>
2024-11-27 20:01:25 +0530

    "Updated CSS styles and JavaScript code, added scroll event listener, and modified HTML templates for header and blogs pages."

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,27 @@
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 100) {
+    header.classList.add("h-[3rem]");
+    header.classList.remove("h-14");
+  } else {
+    header.classList.add("h-14");
+    header.classList.remove("h-[3rem]");
+  }
+});
+
 const addEventOnElements = function (elements, eventType, callback) {
-    for (let i = 0, len = elements.length; i < len; i++) {
-      elements[i].addEventListener(eventType, callback);
-    }
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
   }
-  
-  
-  const navTogglers = document.querySelectorAll("[data-nav-toggler]");
-  const menu = document.querySelector(".menu");
-  
-  const toggleNavbar = function () {
-    menu.classList.toggle("show");
-   
-  }
-  
-  addEventOnElements(navTogglers, "click", toggleNavbar);
+};
+
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const menu = document.querySelector(".menu");
+
+const toggleNavbar = function () {
+  menu.classList.toggle("show");
+};
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
