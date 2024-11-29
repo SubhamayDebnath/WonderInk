359f506b214afd006d7ba277459e86b5c04470f8
Subhamay Debnath <subhamaydebnath@gmail.com>
2024-11-29 22:11:10 +0530

    "Added input validation for name, email, and password in auth controller and updated register.ejs to make name input required"

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -29,14 +29,25 @@ const loginPage = async (req, res) => {
 const register = async (req,res) => {
   try {
     const { name, email, password } = req.body;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{6,26})/;
     if(!name || !email || !password) {
       req.flash("error_msg", "Please fill in all fields");
       return res.redirect("/auth/register");
     }
-    if(name.length < 3 || name.length > 20) {
-      req.flash("error_msg", "Name must be between 3 and 20 characters");
+    if(name.length < 3 || name.length > 30) {
+      req.flash("error_msg", "Name must be between 3 and 30 characters");
       return res.redirect("/auth/register");
     }
+    if(!emailRegex.test(email)){
+      req.flash("error_msg", "Invalid email");
+      return res.redirect("/auth/register");
+    }
+    if(!passwordRegex.test(password)){
+      req.flash("error_msg", "Password must be 6-26 characters long, contain at least one uppercase letter, one special character, and one number");
+      return res.redirect("/auth/register");
+    }
+    
   } catch (error) {
     console.log(`Register error : ${error}`);
     res.redirect('/error')
